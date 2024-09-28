4956b53b887f2a1165d70ea37aec69fa31da3034
Aielina <aielina.madambekova@gmail.com>
2024-09-28 12:32:29 +0600

    Created Header component and added Header component to App

---
+++ src/components/Header.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Header: React.FC = () => {
+    return (
+        <header className='bg-light p-3 text-center'>
+            <h1>Food court</h1>
+        </header>
+    );
+};
+
+export default Header;
