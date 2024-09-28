97597bd8e59dd82c5e4891cd0a4e3403ac244d96
Aielina <aielina.madambekova@gmail.com>
2024-09-28 14:09:20 +0600

    Create Menu component and display items

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,21 @@
 import './App.css';
+import React, {useState} from 'react';
 import Header from './components/Header';
+import Menu from './components/Menu';
 
 const App: React.FC = () => {
+    const [order, setOrder] = useState<{ name:string, price:number } []>([]);
+
+    const handleAddItem = (name: string, price:number) => {
+        setOrder([...order,{name, price}]);
+    };
+
     return(
         <div className="App">
             <Header />
+            <div className='container mt-3'>
+                <Menu onAddItem={handleAddItem} />
+            </div>
         </div>
     );
 };
---
+++ src/components/Menu.tsx
@@ -0,0 +1,32 @@
+import React from "react";
+import MenuItem from "./MenuItem";
+
+type MenuProps = {
+    onAddItem: (name: string, price: number) => void;
+};
+
+const Menu: React.FC<MenuProps> = ({ onAddItem }) => {
+    const items = [
+        {name: 'Hamburger', price: 80},
+        {name: 'Coffee', price: 70},
+        {name: 'Cheeseburger', price: 90},
+        {name: 'Tea', price: 50},
+        {name: 'Fries', price: 45},
+        {name: 'Cola', price: 40},
+    ];
+
+
+    return (
+        <div className="d-flex flex-wrap">
+            {items.map((item) => (
+                <MenuItem
+                key={item.name}
+                name={item.name}
+                price={item.price}
+                onAdd={() => onAddItem(item.name, item.price)} />
+            ))}
+        </div>
+    );
+};
+
+export default Menu;
